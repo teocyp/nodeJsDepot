4c434c51c7e5704b6a4d741c943abf11e9aa3354
teocyp <teocyp1@gmail.com>
2021-09-02 08:53:51 +0300

    NodeJS Practice Cont..

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(() => {
+app.use((req, res) => {
     console.log('We got a new request');
+    con
 })
 app.listen(8080, () => {
     console.log("Listening on port 8080!")

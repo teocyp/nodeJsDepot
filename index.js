737b6fa4788c267343466df862ca3920fb79d67e
teocyp <teocyp1@gmail.com>
2021-09-02 09:07:30 +0300

    NodeJS Practice Cont..

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require("express");
-const app = express();
+// Express for NodeJS..
+const express = require("express"); // Requires the modules to work with the framework into this file.
+const app = express(); // Executes the express
 
-app.use((req, res) => {
-    console.log('We got a new request');
-    con
+
+// Get Request
+app.get('/cats', (req, res) => {
+    res.send('Meooow!')
 })
+
+app.get('/', (req, res) => {
+    res.send('This is the home page!')
+})
+
+// Post Request
+app.post('/', (req, res) => {
+    res.send('This is a post request!')
+})
+
+// Starts the server on port number localhost:8080
 app.listen(8080, () => {
     console.log("Listening on port 8080!")
 })
